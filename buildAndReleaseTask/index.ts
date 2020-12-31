2d3adfdce7ca05eb23d28d19015d6408cba200b1
rilambarithy <rilambarithy@gmail.com>
2020-12-31 06:03:26 +0530

    Rest update

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -2,16 +2,28 @@ import tl = require('azure-pipelines-task-lib/task');
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
+        const userName: string | undefined = tl.getInput('userName', true);
+        if (userName == 'bad') {
             tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
             return;
         }
-        console.log('Hello', inputString);
+        console.log('Hello', userName);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
+    try {
+        const id: string | undefined = tl.getInput('instanceId', true);
+        if (id == 'bad') {
+            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+            return;
+        }
+        console.log('Initiating Rest Call for ID ', id);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+
 }
 
 run();
